0aeeb04e2c48dfd724b7a6648a0318a9b8adeb85
mani1 <mani1@NANI143>
2022-08-02 12:20:48 +0530

    Added program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparision_EqualityOfLine
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate The Equality Of Line");
+            UC2_Equallity lc = new UC2_equality();
+            ec.Equalitycal();
+        }
+    }
+}
